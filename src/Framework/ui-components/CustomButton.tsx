d4c823007e09e36e5f5db0ae7e5430bc22fdc7a5
karthik7013 <karthiktumala143@gmail.com>
2025-02-16 01:45:22 +0530

    added button-ui

--- src/Framework/ui-components/CustomButton.tsx
+++ src/Framework/ui-components/CustomButton.tsx
@@ -1,7 +1,7 @@
 import { alpha, styled } from "@mui/material/styles"
 import { Button } from '@mui/material'
 const CustomButton = styled(Button)(({ theme }) => ({
-
+    backgroundColor: `${theme.palette.primary.main}`
 }))
 
 
