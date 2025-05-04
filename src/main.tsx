4c9087afd11d02864098793fb4043f78d8f7cff1
karthik7013 <karthiktumala143@gmail.com>
2025-05-04 22:10:59 +0530

    added code

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (import.meta.env.VITE_NODE_ENV === 'PRODUCTION') {
+  console.log = () => { };
+  console.warn = () => { };
+  console.error = () => { };
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
 )
