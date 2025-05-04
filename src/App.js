c14b60e453eede54f1ac52666d9b82d2e11f8cd0
karthik7013 <karthiktumala143@gmail.com>
2025-05-04 12:57:26 +0530

    rm all code

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import React from 'react';
-//============ MUI IMPORTS ==============>
-import { CssBaseline } from '@mui/material';
-//============ PROJECT IMPORTS ==============>
-const App = () => {
-    return (_jsxs(_Fragment, { children: [_jsx(CssBaseline, {}), _jsx(React.Suspense, { fallback: "...loading" })] }));
-};
-export default App;
