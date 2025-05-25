a9607cba3867c3583a253388284485979b226240
karthik7013 <karthiktumala143@gmail.com>
2025-05-25 09:42:49 +0530

    added changes

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios'
 type props = {
     children: React.ReactNode
 }
 const AuthProvider = (props: props) => {
+    const [step1, setStep1] = useState(false)
     const setCache = async () => {
         const res = await axios.get('https://namelix.koyeb.app/setCookie');
-        alert(JSON.stringify(res.data))
+        alert(JSON.stringify(res.data));
+        setStep1(true)
+    }
+    const getCache = async () => {
+        const res = await axios.get('https://namelix.koyeb.app/getCookie');
+        console.log(res.data)
     }
     useEffect(() => {
-        setCache()
+        setCache();
+
     }, [])
+    useEffect(() => {
+        if (step1) {
+            getCache()
+        }
+    }, [step1])
     return <>{props.children}</>
 }
 export default AuthProvider;
