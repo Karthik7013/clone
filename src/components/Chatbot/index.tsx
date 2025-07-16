e25e0a5952bebfebe4f15eb4608e558717306b84
Karthik7013 <karthiktumala143@gmail.com>
2025-07-16 15:59:45 +0530

    added select model

--- src/components/Chatbot/index.tsx
+++ src/components/Chatbot/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import chat_bot from "../../assets/images/gemini_ai_.svg";
-import { Alert, Avatar, Box, Button, Card, CardActions, CardContent, CircularProgress, Collapse, Divider, IconButton, keyframes, List, ListItem, ListItemIcon, ListItemText, Menu, MenuItem, Paper, Skeleton, Stack, TextField, Typography } from '@mui/material';
-import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
+import { Alert, Avatar, Box, Button, Card, CardActions, CardContent, CircularProgress, Collapse, Divider, FormControl, IconButton, keyframes, List, ListItem, ListItemIcon, ListItemText, Menu, MenuItem, Paper, Select, Skeleton, Stack, TextField, Typography } from '@mui/material';
 import { AppDispatch, RootState } from '../../store/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
@@ -28,7 +27,8 @@ type conversationProps = {
     timeStamp: string
 }
 interface BotSubmitType {
-    t: string
+    t: string,
+    model: string
 }
 
 function isFetchBaseQueryError(
@@ -48,7 +48,8 @@ const Chatbot = () => {
     const dispatch: AppDispatch = useDispatch()
     const { handleSubmit, control, reset } = useForm<BotSubmitType>({
         defaultValues: {
-            t: ''
+            t: '',
+            model: 'models/gemini-2.5-flash'
         }
     })
     const conversation = useSelector((state: RootState) => state.chatbotReducer.conversation);
@@ -74,6 +75,7 @@ const Chatbot = () => {
 
 
     useEffect(() => {
+        console.log('error', error)
         setErrorVisible(error);
     }, [error]);
     const Conversation = ({ candidate, response }: conversationProps) => {
@@ -133,16 +135,201 @@ const Chatbot = () => {
   }
 `;
 
+    // const models = [
+    //     {
+    //         label: 'Gemini 2.5 Flash',
+    //         value: 'gemini-2.5-flash'
+    //     },
+    //     {
+    //         label: 'Gemini 2.5 Flash Lite Preview 06-17',
+    //         value: 'gemini-2.5-flash-lite-preview-06-17'
+    //     },
+    //     {
+    //         label: 'Gemini 1.5 Pro',
+    //         value: 'gemini-1.5-pro'
+    //     }
+    // ]
+    const models = [
+        { label: 'Embedding Gecko', value: 'models/embedding-gecko-001' },
+        {
+            label: 'Gemini 1.0 Pro Vision',
+            value: 'models/gemini-1.0-pro-vision-latest'
+        },
+        { label: 'Gemini 1.0 Pro Vision', value: 'models/gemini-pro-vision' },
+        {
+            label: 'Gemini 1.5 Pro Latest',
+            value: 'models/gemini-1.5-pro-latest'
+        },
+        { label: 'Gemini 1.5 Pro 002', value: 'models/gemini-1.5-pro-002' },
+        { label: 'Gemini 1.5 Pro', value: 'models/gemini-1.5-pro' },
+        {
+            label: 'Gemini 1.5 Flash Latest',
+            value: 'models/gemini-1.5-flash-latest'
+        },
+        { label: 'Gemini 1.5 Flash', value: 'models/gemini-1.5-flash' },
+        {
+            label: 'Gemini 1.5 Flash 002',
+            value: 'models/gemini-1.5-flash-002'
+        },
+        { label: 'Gemini 1.5 Flash-8B', value: 'models/gemini-1.5-flash-8b' },
+        {
+            label: 'Gemini 1.5 Flash-8B 001',
+            value: 'models/gemini-1.5-flash-8b-001'
+        },
+        {
+            label: 'Gemini 1.5 Flash-8B Latest',
+            value: 'models/gemini-1.5-flash-8b-latest'
+        },
+        {
+            label: 'Gemini 2.5 Pro Preview 03-25',
+            value: 'models/gemini-2.5-pro-preview-03-25'
+        },
+        {
+            label: 'Gemini 2.5 Flash Preview 05-20',
+            value: 'models/gemini-2.5-flash-preview-05-20'
+        },
+        { label: 'Gemini 2.5 Flash', value: 'models/gemini-2.5-flash' },
+        {
+            label: 'Gemini 2.5 Flash-Lite Preview 06-17',
+            value: 'models/gemini-2.5-flash-lite-preview-06-17'
+        },
+        {
+            label: 'Gemini 2.5 Pro Preview 05-06',
+            value: 'models/gemini-2.5-pro-preview-05-06'
+        },
+        {
+            label: 'Gemini 2.5 Pro Preview',
+            value: 'models/gemini-2.5-pro-preview-06-05'
+        },
+        { label: 'Gemini 2.5 Pro', value: 'models/gemini-2.5-pro' },
+        {
+            label: 'Gemini 2.0 Flash Experimental',
+            value: 'models/gemini-2.0-flash-exp'
+        },
+        { label: 'Gemini 2.0 Flash', value: 'models/gemini-2.0-flash' },
+        {
+            label: 'Gemini 2.0 Flash 001',
+            value: 'models/gemini-2.0-flash-001'
+        },
+        {
+            label: 'Gemini 2.0 Flash (Image Generation) Experimental',
+            value: 'models/gemini-2.0-flash-exp-image-generation'
+        },
+        {
+            label: 'Gemini 2.0 Flash-Lite 001',
+            value: 'models/gemini-2.0-flash-lite-001'
+        },
+        {
+            label: 'Gemini 2.0 Flash-Lite',
+            value: 'models/gemini-2.0-flash-lite'
+        },
+        {
+            label: 'Gemini 2.0 Flash Preview Image Generation',
+            value: 'models/gemini-2.0-flash-preview-image-generation'
+        },
+        {
+            label: 'Gemini 2.0 Flash-Lite Preview 02-05',
+            value: 'models/gemini-2.0-flash-lite-preview-02-05'
+        },
+        {
+            label: 'Gemini 2.0 Flash-Lite Preview',
+            value: 'models/gemini-2.0-flash-lite-preview'
+        },
+        {
+            label: 'Gemini 2.0 Pro Experimental',
+            value: 'models/gemini-2.0-pro-exp'
+        },
+        {
+            label: 'Gemini 2.0 Pro Experimental 02-05',
+            value: 'models/gemini-2.0-pro-exp-02-05'
+        },
+        {
+            label: 'Gemini Experimental 1206',
+            value: 'models/gemini-exp-1206'
+        },
+        {
+            label: 'Gemini 2.5 Flash Preview 05-20',
+            value: 'models/gemini-2.0-flash-thinking-exp-01-21'
+        },
+        {
+            label: 'Gemini 2.5 Flash Preview 05-20',
+            value: 'models/gemini-2.0-flash-thinking-exp'
+        },
+        {
+            label: 'Gemini 2.5 Flash Preview 05-20',
+            value: 'models/gemini-2.0-flash-thinking-exp-1219'
+        },
+        {
+            label: 'Gemini 2.5 Flash Preview TTS',
+            value: 'models/gemini-2.5-flash-preview-tts'
+        },
+        {
+            label: 'Gemini 2.5 Pro Preview TTS',
+            value: 'models/gemini-2.5-pro-preview-tts'
+        },
+        {
+            label: 'LearnLM 2.0 Flash Experimental',
+            value: 'models/learnlm-2.0-flash-experimental'
+        },
+        { label: 'Gemma 3 1B', value: 'models/gemma-3-1b-it' },
+        { label: 'Gemma 3 4B', value: 'models/gemma-3-4b-it' },
+        { label: 'Gemma 3 12B', value: 'models/gemma-3-12b-it' },
+        { label: 'Gemma 3 27B', value: 'models/gemma-3-27b-it' },
+        { label: 'Gemma 3n E4B', value: 'models/gemma-3n-e4b-it' },
+        { label: 'Gemma 3n E2B', value: 'models/gemma-3n-e2b-it' },
+        { label: 'Embedding 001', value: 'models/embedding-001' },
+        { label: 'Text Embedding 004', value: 'models/text-embedding-004' },
+        {
+            label: 'Gemini Embedding Experimental 03-07',
+            value: 'models/gemini-embedding-exp-03-07'
+        },
+        {
+            label: 'Gemini Embedding Experimental',
+            value: 'models/gemini-embedding-exp'
+        },
+        {
+            label: 'Gemini Embedding 001',
+            value: 'models/gemini-embedding-001'
+        },
+        {
+            label: 'Model that performs Attributed Question Answering.',
+            value: 'models/aqa'
+        },
+        {
+            label: 'Imagen 3.0 002 model',
+            value: 'models/imagen-3.0-generate-002'
+        }
+    ]
+
     return (
-        <Stack height={"100%"} position={'relative'} bgcolor={'background.paper'}>
+        <Stack height={"100%"} position={'relative'} bgcolor={'background.paper'} component='form' onSubmit={handleSubmit(onHandleSubmit)}>
             {/* header */}
             <Box position={'sticky'} top={0} left={0}>
                 <CardContent>
                     <ListItem disableGutters disablePadding
                         secondaryAction={
-                            <IconButton edge="end" aria-label="delete">
-                                <OpenInNewRoundedIcon />
-                            </IconButton>
+                            <FormControl size="small">
+                                <Controller
+                                    name="model"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Select
+                                            size='small'
+                                            sx={{ width: '200px', borderRadius }}
+                                            labelId="Model-label"
+                                            {...field}
+                                        >
+                                            {
+                                                models.map((models) => {
+                                                    return <MenuItem key={models.value} value={models.value}>
+                                                        {models.label}
+                                                    </MenuItem>
+                                                })
+                                            }
+                                        </Select>
+                                    )}
+                                />
+                            </FormControl>
                         }
                     >
                         <ListItemIcon>
@@ -168,7 +355,7 @@ const Chatbot = () => {
             }}>
                 {!conversation.length && <Box height={'100%'} display='flex' alignItems='center' flexDirection='column' justifyContent='space-between'>
                     <Stack gap={2} justifyContent={'center'} width={'100%'} flexGrow={1} >
-                        <Typography variant='h4' fontWeight={600} textAlign='center'>🖐 Hi there<br /> how can i help you today ?</Typography>
+                        <Typography variant='h4' textAlign='center'>🖐 Hi there<br /> how can i help you today ?</Typography>
                         <Typography variant='caption' fontWeight={600} textAlign='center'>Ready to help.</Typography>
                     </Stack>
                 </Box>}
@@ -180,26 +367,27 @@ const Chatbot = () => {
                 </List>
             </Box>
             {/* footer */}
-            <Box position={'sticky'} bottom={0} left={0} component='form' onSubmit={handleSubmit(onHandleSubmit)}>
+            <Box position={'sticky'} bottom={0} left={0}>
                 <Collapse in={Boolean(errorVisible)} unmountOnExit orientation='vertical'>
+                    <Alert
+                        variant="filled"
+                        severity="error"
+                        sx={{ width: '100%', borderRadius, mb: 0.5 }}
+                        action={
+                            <Button size="small" onClick={handleClose} color="inherit">
+                                Close
+                            </Button>
+                        }
+                    >
+                        {(isFetchBaseQueryError(error) &&
+                            typeof error.data === 'object' &&
+                            error.data !== null &&
+                            'message' in error.data &&
+                            typeof error.data.message === 'string')
+                            ? error.data.message
+                            : "An error occurred while processing your request."}
+                    </Alert>
 
-                    {isFetchBaseQueryError(error) &&
-                        typeof error.data === 'object' &&
-                        error.data !== null &&
-                        'message' in error.data && typeof error.data.message === 'string' && (
-                            <Alert
-                                variant="filled"
-                                severity="error"
-                                sx={{ width: '100%', borderRadius, mb: 0.5 }}
-                                action={
-                                    <Button size="small" onClick={handleClose} color="inherit">
-                                        Close
-                                    </Button>
-                                }
-                            >
-                                {error.data.message}
-                            </Alert>
-                        )}
 
                 </Collapse>
                 <Card elevation={1} sx={{ borderRadius, borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}>
