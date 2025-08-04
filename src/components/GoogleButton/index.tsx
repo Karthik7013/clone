c5590d9c89b0a609a5df2ad12eee53972a1db21f
karthik7013 <karthiktumala143@gmail.com>
2025-08-04 21:21:34 +0530

    add login

--- src/components/GoogleButton/index.tsx
+++ src/components/GoogleButton/index.tsx
@@ -1,8 +1,24 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Avatar } from "@mui/material";
+
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
-
-    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+    const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+    console.log(user);
+    return <div>
+        {!isAuthenticated ? (
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+        ) : (
+            <div>
+                <img src={user?.picture} alt={user?.name} />
+                <h2>{user?.name}</h2>
+                <p>{user?.email}</p>
+                <Avatar src={user?.picture} alt="pic" />
+                {/* <button onClick={() => logout({ returnTo: window.location.origin })}>
+                    Log Out
+                </button> */}
+            </div>
+        )}
+    </div>
 };
 export default LoginButton;
