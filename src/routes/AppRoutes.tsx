b257bc597a1dd809e1f45898541bd2ae8e9b10f1
karthik7013 <karthiktumala143@gmail.com>
2025-08-10 17:28:56 +0530

    added changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,18 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../layouts/AppLayout";
 import PageNotFound from "../components/PageNotFound";
-import { Container } from "@mui/material";
 import Chatbot from "../components/Chatbot";
 
 const AppRoutes = () => {
     return <Routes>
         <Route path='/' element={<AppLayout />} />
         <Route path='/chat' element={
-            <Container
-                maxWidth={'md'}
-                sx={{ height: '100dvh', padding: 0 }}>
+        
                 <Chatbot />
-            </Container>
         } />
         <Route path='*' element={<PageNotFound />} />
     </Routes>
