d2f277865ad34c29d9cc3962ae774bb00baca5a8
karthik7013 <karthiktumala143@gmail.com>
2025-09-01 23:38:51 +0530

    loader changed

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,11 +1,37 @@
-import { Backdrop, CircularProgress } from "@mui/material";
+import { Backdrop, keyframes, Typography } from "@mui/material";
+import GeminiIcon from "../../assets/icons/GeminiIcon";
+import AnimatedWrapper from "../AnimatedWrapper";
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  40% {
+    transform: rotate(180deg);
+  }
+  60% {
+    transform: rotate(200deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 const Loader = () => {
     return <Backdrop
-        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        sx={(theme) => ({
+            color: theme.palette.background.paper, zIndex: theme.zIndex.drawer + 1,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 1
+        })}
         open={true}
     >
-        <CircularProgress color="inherit" />
+        <AnimatedWrapper sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} animation={rotate} duration="2s" timingFunction="ease-in-out">
+            <GeminiIcon fontSize="large" />
+        </AnimatedWrapper>
+        <Typography color='text.secondary'>
+            Starting things...
+        </Typography>
     </Backdrop>
 
 }
